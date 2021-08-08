f62ec24761818490715d7f1f7de0e3ebf4ed03b1
Aaron <aaron.low@motional.com>
2021-08-08 14:10:39 +0800

    Tidying up

--- app/run.py
+++ app/run.py
@@ -1,3 +1,4 @@
+import nltk
 import json
 import joblib
 import plotly
@@ -12,11 +13,47 @@ from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar, Histogram
 from sqlalchemy import create_engine
 
+from sklearn.base import BaseEstimator,TransformerMixin
 
 parser = argparse.ArgumentParser(description='Run web app')
 parser.add_argument('database_filepath', type=str, help='Path to database')
 parser.add_argument('model_filepath', type=str, help='Path to saved model')
 args = parser.parse_args()    
+
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+    """
+    Custom transformer class that checks if the sentence starts with a verb
+    """
+    def starting_verb(self, text):
+        """
+        Checks if the starting word of text is a verb
+        Input:
+            text: Input text
+        Output:
+            True if starting word in text is a verb else False
+        """
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        """
+        Apply transform. Overloaded function.
+        Input:
+            X: input text data
+        Output:
+            Input text data transformed by `starting_verb`
+        """
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
 
 
 def tokenize(text):
@@ -52,7 +89,7 @@ def index():
     category_names = list(category_sum.index)
     
     # Graph 2 -- Message length
-    df["message_len"] = df["message"].apply(lambda x: len(x))
+    df["message_len"] = df["message"].apply(lambda x: len(tokenize(x)))
 
     # create visuals
     graphs = [
